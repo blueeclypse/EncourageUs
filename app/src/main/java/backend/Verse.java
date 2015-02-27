c74a73f07aa6b41261f641356781494b5ac5889f
Hoang <yamatoeclypse@gmail.com>
2015-02-27 17:49:29 -0500

    Added class files and basic android program

---
+++ app/src/main/java/backend/Verse.java
@@ -0,0 +1,33 @@
+package backend;
+
+/**
+ * Created by Hoang on 2/23/2015.
+ */
+public class Verse {
+    private String verse = ""; //stores the verse
+    private int chapter = 1; //chapter number
+    private int verseNum = 1; //verse number
+
+    public Verse(String verse, int chapter, int verseNum){
+        this.verse = verse;
+        this.chapter = chapter;
+        this.verseNum = verseNum;  
+    }
+
+    //returns the verse
+    public int getChapter(){
+        return chapter;
+    }
+
+    //returns the verse
+    public int getVerseNum(){
+        return verseNum;
+    }
+
+    //returns the verse
+    public String getVerse(){
+        return verse;
+    }
+
+
+}
